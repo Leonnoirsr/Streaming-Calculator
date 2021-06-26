822c2c4b4932439c35eb47c94b6f08a11456b871
Leonnoirsr <huggins.leon@gmail.com>
2021-06-25 22:10:36 -0400

    Added more streaming platforms to select menu and values to JS file

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const calc = document.querySelector('button');
 const tidal = 0.01284;
 const appleMusic = 0.01;
 const spotify = 0.00437;
+const napster = 0.019;
+const deezer = 0.0064;
+const amazon = 0.00402;
+const youtube = 0.00069;
+const pandora = 0.00133;
 
 calc.addEventListener('click', function () {
 	const streamingServices = document.querySelector('select').value;
@@ -20,6 +25,21 @@ calc.addEventListener('click', function () {
 			break;
 		case 'spotify':
 			money.innerHTML = `You earned: $${streamNum.value * spotify.toFixed(4)}`;
+			break;
+		case 'napster':
+			money.innerHTML = `You earned: $${streamNum.value * napster.toFixed(4)}`;
+			break;
+		case 'deezer':
+			money.innerHTML = `You earned: $${streamNum.value * deezer.toFixed(4)}`;
+			break;
+		case 'amazon':
+			money.innerHTML = `You earned: $${streamNum.value * amazon.toFixed(4)}`;
+			break;
+		case 'youtube':
+			money.innerHTML = `You earned: $${streamNum.value * youtube.toFixed(4)}`;
+			break;
+		case 'pandora':
+			money.innerHTML = `You earned: $${streamNum.value * pandora.toFixed(4)}`;
 			break;
 	}
 });
