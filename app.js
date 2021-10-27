0923abd82208968441de20b1508f3334cd245713
Leon Huggins <huggins.leon@gmail.com>
2021-10-26 21:15:10 -0400

    Refactored calculation event listener added functions to make DRY

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const streamNum = document.getElementById('streams');
 const money = document.querySelector('h2');
 const calc = document.querySelector('button');
+const imgRef = document.querySelector('select');
 
 const platforms = {
 tidal: 0.01284,
@@ -10,51 +11,49 @@ napster: 0.019,
 deezer: 0.0064,
 amazon: 0.00402,
 youtube: 0.00069,
-pandora: 0.00133
+pandora: 0.00133,
+getMoney: (streamNum, platform) => streamNum * platform
 }
 
+// const platformImages = {
+// 	tidal: 'Assets/tidal.png',
+// appleMusic: 'Assets/applemusic.png',
+// spotify: 'Assets/spotify.png',
+// napster: 'Assets/napster.png',
+// deezer: 'Assets/deezer.png',
+// amazon: 'Assets/amazon.png',
+// youtube: 'Assets/youtube.png',
+// pandora: 'Assets/pandora.png',
+
+// getImg: () => imgRef 
+// }
 
 calc.addEventListener('click', () => {
 	const streamingServices = document.querySelector('select').value;
+	const showMoney = (streamingServices) => {
+	
+		const amount =  platforms[`${streamingServices}`] < 0.01 ? platforms[`${streamingServices}`].toFixed(4) : platforms[`${streamingServices}`]
+		return money.innerHTML = `You earned: $${platforms.getMoney(streamNum.value, amount)}` 
 
-	switch (streamingServices) {
-		case 'tidal':
-			money.innerHTML = `You earned: $${streamNum.value * platforms.tidal.toFixed(4)}`;
-			break;
-		case 'apple music':
-			money.innerHTML = `You earned: $${
-				streamNum.value * platforms.appleMusic.toFixed(4)
-			}`;
-			break;
-		case 'spotify':
-			money.innerHTML = `You earned: $${streamNum.value * platforms.spotify.toFixed(4)}`;
-			break;
-		case 'napster':
-			money.innerHTML = `You earned: $${streamNum.value * platforms.napster.toFixed(4)}`;
-			break;
-		case 'deezer':
-			money.innerHTML = `You earned: $${streamNum.value * platforms.deezer.toFixed(4)}`;
-			break;
-		case 'amazon':
-			money.innerHTML = `You earned: $${streamNum.value * platforms.amazon.toFixed(4)}`;
-			break;
-		case 'youtube':
-			money.innerHTML = `You earned: $${streamNum.value * platforms.youtube.toFixed(4)}`;
-			break;
-		case 'pandora':
-			money.innerHTML = `You earned: $${streamNum.value * platforms.pandora.toFixed(4)}`;
-			break;
 	}
+	showMoney(streamingServices)
 });
 
-const imgRef = document.querySelector('select');
+
+
 
 imgRef.addEventListener('change', (e) => {
 	e.preventDefault();
 	const img = document.querySelector('img');
+// const showImg = (img) => {
+// 	if (imgRef) {
+// 		img.src = `Assets/${imgRef}.png`
+// 	}
+// }
+
 	if (imgRef.value == 'tidal') {
 		img.src = "Assets/tidal.png"
-	} else if (imgRef.value == 'apple music') {
+	} else if (imgRef.value == 'appleMusic') {
 		img.src = 'Assets/applemusic.png'
 	} else if (imgRef.value == 'spotify') {
 		img.src = "Assets/spotify.png"
